245c671cc7a066285840a481a2acc4632c554c41
Giuseppe <giuseppe@osgate.org>
2012-03-16 08:30:02 +0100

    Modified GUI

--- monopoly/src/common/MonopolyGUI.java
+++ monopoly/src/common/MonopolyGUI.java
@@ -6,7 +6,12 @@ import java.awt.Color;
 import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.TextArea;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -16,6 +21,8 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
@@ -42,16 +49,46 @@ public class MonopolyGUI extends JFrame {
 	 * @return the left jpanel
 	 */
 	private JPanel leftPanel(){
-		JPanel left = new JPanel(new GridLayout(2,1));
-		//left.setBorder(BorderFactory.createEtchedBorder());
+		JPanel left = new JPanel(new GridLayout(4,1));
 		JPanel info = new JPanel();
 		info.setBorder(BorderFactory.createEtchedBorder());
-		info.add(new JLabel("kdhgdslhgdlhgdlhfgldhgd"));
-		
-		left.add(info);
+
+		left.add(cardPanel());
 		left.add(bottomPanel());
+		left.add(infoPanel());
+		left.add(historyChatPanel());
 
 		return left;
+	}
+
+	private JPanel infoPanel(){
+		JPanel info = new JPanel();
+		info.add(new JLabel("Player 1: 100000$"));
+		return info;
+	}
+
+	private JTabbedPane cardPanel(){
+		JTabbedPane card = new JTabbedPane();
+		JPanel tab1 = new JPanel();
+		JPanel tab2 = new JPanel();
+		card.addTab("Front", tab1);
+		card.addTab("Rear", tab2);
+		card.setBorder(BorderFactory.createEtchedBorder());
+		card.setSize(new Dimension(100,100));
+		return card;
+	}
+
+	private JTabbedPane historyChatPanel(){
+		JTabbedPane pane = new JTabbedPane();
+		pane.setBorder(BorderFactory.createEtchedBorder());
+		pane.setSize(new Dimension(100,100));
+		TextArea history = new TextArea(5,25);
+		JScrollPane scroll = new JScrollPane(history);
+		TextArea chat = new TextArea(5,25);
+		JScrollPane scrollChat = new JScrollPane(chat);
+		pane.addTab("Chat", scrollChat);
+		pane.addTab("History", scroll);
+		return pane;
 	}
 
 	/**
@@ -108,39 +145,40 @@ public class MonopolyGUI extends JFrame {
 	 * @return
 	 */
 	private JPanel drawBoard(){
-		GridLayout cellTable = new GridLayout(11,11, 0, 0);
-		JPanel board = new JPanel(cellTable);
-		board.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		GridBagLayout layout = new GridBagLayout();
 
+		JPanel board = new JPanel(layout);
 		JPanel monopolyTiles[][] = new JPanel[11][11];
 
-		int j,i;
+		for(int j = 0 ; j < 11 ; j++){
+			for(int i = 0 ; i < 11 ; i++){
+				monopolyTiles[j][i] = new JPanel();
+				monopolyTiles[j][i].setBorder(BorderFactory.createEtchedBorder());
 
-		for(j = 0 ; j < 11 ; j++){
-			for(i = 0 ; i < 11 ; i++){
-				if(j==0 || j==10 || i == 0 || i == 10) 
-					monopolyTiles[j][i] = new JPanel();
-				else
-					monopolyTiles[j][i] = new JPanel();
-			}
-		}
-
-		for(j = 0 ; j < 11 ; j++){
-			for(i = 0 ; i < 11 ; i++){
-				if(j == 0 || j == 10 || i == 0 || i == 10){
-					monopolyTiles[j][i].setBorder(BorderFactory.createEtchedBorder());
-					JLabel name = new JLabel("<html>Meditterrean<br>avenue</html>");
-					Font myFont = new Font(name.getFont().getFontName(), name.getFont().getStyle(),10);
-					name.setFont(myFont);
-					monopolyTiles[j][i].add(name);
-					board.add(monopolyTiles[j][i]);
+				if(j == 0 && i == 0 || j == 10 && i == 0 || j == 0 && i == 10 || j == 10 && i == 10){
+					monopolyTiles[j][i].add(new JLabel("Corner"));
+					monopolyTiles[j][i].setBorder(BorderFactory.createRaisedBevelBorder());
+					board.add(monopolyTiles[j][i], new GridBagConstraints(j, i, 1, 1, 0.12, 0.12, 
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+				}
+				else if(j == 1 && i == 1){
+					JPanel main = new JPanel();
+					main.add(new JLabel("hi"));
+					main.setSize(new Dimension(300,200));
+					main.setBackground(Color.GREEN);
+					monopolyTiles[j][i].add(main);
+					monopolyTiles[j][i].setBackground(Color.GREEN);
+					board.add(monopolyTiles[j][i], new GridBagConstraints(j, i, 9,9, 0.1, 0.1, 
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 				}
 				else{
-					monopolyTiles[j][i].setBackground(Color.WHITE);
-					board.add(monopolyTiles[j][i]);
+					monopolyTiles[j][i].add(new JLabel("Tile"));
+					board.add(monopolyTiles[j][i], new GridBagConstraints(j, i, 1, 1, 0.1, 0.1, 
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 				}
 			}
 		}
+
 
 		return board;
 	}
