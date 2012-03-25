e9a3c0923f7cc1dd07515f853a3efdbb04a62704
Justin S <shrevek@gmail.com>
2012-03-25 17:54:36 +0200

    added more to the basic data/state classes

--- monopoly/src/common/StateManager.java
+++ monopoly/src/common/StateManager.java
@@ -1,5 +1,22 @@
 package common;
 
-public class StateManager {
+import java.awt.Color;
+import java.net.Socket;
 
+public class StateManager {
+	Player[] players;
+	private final int START_MONEY_US = 5000;
+	private final int START_MONEY_SWISS = 200000;
+	
+
+	// TODO: how do we get the player's names and selected colors to this point?
+	public StateManager (int numberOfPlayers) {
+		players = new Player[numberOfPlayers];
+		for (int i =0; i<numberOfPlayers;i++){
+			
+			//constructor takes params :  String name, Color color, int account, Socket socket
+			players[i] = new Player(null, null, START_MONEY_US, null);
+			
+		}
+	}
 }
