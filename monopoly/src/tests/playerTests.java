9d366d2713cbe4448b01f4604fe98d4264b128ad
Justin S <shrevek@gmail.com>
2012-03-25 20:31:31 +0200

    more development of basic classes for program structure

--- monopoly/src/tests/playerTests.java
+++ monopoly/src/tests/playerTests.java
@@ -1,5 +1,32 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Color;
+
+import org.junit.Before;
+import org.junit.Test;
+import common.GameServer;
+import common.Player;
+
+
+
+
 public class playerTests {
 
+	@Before
+	public void setup(){
+
+	
+	}
+	
+	@Test
+	public void playerAttributesSaveCorrectly() {
+		Color blue = Color.BLUE;
+		Player p = new Player("Justin", blue, 5000, null);
+		assertTrue(p.getName().equals("Justin"));
+		assertTrue(p.getAccount() == 5000);
+		
+	}
+	
 }
