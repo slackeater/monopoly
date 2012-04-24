5b7a1bbc1e28aab13edb90e60b302af4bf39f299
Justin S <shrevek@gmail.com>
2012-04-24 19:32:00 +0200

    great, lost changes

---
+++ monopoly/src/ch/bfh/monopoly/event/EventManager.java
@@ -0,0 +1,12 @@
+package ch.bfh.monopoly.event;
+
+import ch.bfh.monopoly.common.GameClient;
+
+public class EventManager {
+	public EventManager(GameClient gameClient) {
+		// TODO Auto-generated constructor stub
+	}
+
+	TileEvent[] masterList;
+	
+}
